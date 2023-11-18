45ac780f59a9657c06d16e916cc0b6148ab846ad
Jun <59083443+Junch25@users.noreply.github.com>
2023-11-18 22:07:09 +0900

    Update driver-install.sh

--- driver/driver-install.sh
+++ driver/driver-install.sh
@@ -6,5 +6,5 @@ FILENAME="ubuntu20.04.2_PX-S1UD_Driver"
 
 echo "Driver install start!"
 unzip "$FILENAME".zip
-sudo cp "$FILENAME"/s270-firmware/isdbt_rio.inp /lib/firmware/
+sudo cp s270-firmware/isdbt_rio.inp /lib/firmware/
 echo "done!"
